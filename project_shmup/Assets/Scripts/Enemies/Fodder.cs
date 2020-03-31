231bed17a4b0b2d7b22d3cd73acbf8b5317f6e85
maxz2002 <mz13223@gmail.com>
2020-03-31 17:42:13 -0400

    fodder moves back and forth

--- project_shmup/Assets/Scripts/Enemies/Fodder.cs
+++ project_shmup/Assets/Scripts/Enemies/Fodder.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Fodder : Enemy
 {
+    int direction = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class Fodder : Enemy
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        if (transform.position.x >= 15 || transform.position.x <= -15)
+        {
+            direction = direction * -1;
+        }
+        transform.position = transform.position + new Vector3(moveSpeed * Time.deltaTime * direction, 0, 0);
     }
 }
