ba2c8dc0a68f913fd9d965382bb12ebba5dc4572
wennethkang <kennethzwang@gmail.com>
2020-03-31 18:42:35 -0400

    player movement completed and player bounds created

--- project_shmup/Assets/Scripts/Player/PlayerMovement.cs
+++ project_shmup/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     float speed = 12.0f; // player speed
     Vector3 move; // directional vector
+    bool holdingTwoHori = false; // checks if the player is holding both horizontal directions
+    bool holdingTwoVert = false; // ^ for vertical
     
     // Controls: arrows for normal movement
     // Z and X for bomb and shoot
@@ -17,11 +19,99 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        // player movement calculations start here (p much near perfect movement, works better on WASD than arrow keys for some reason) ////////////////////////////////////////////////////////
+        Vector3 prev = move;
+        move = Vector3.zero;
+        bool leftPressed = false;
+        bool rightPressed = false;
+        bool upPressed = false;
+        bool downPressed = false;
+        
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            leftPressed = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rightPressed = true;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            upPressed = true;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            downPressed = true;
+        }
+        if (leftPressed && rightPressed)
+        {
+            if(holdingTwoHori)
+            {
+                move.x = prev.x;
+            } else
+            {
+                move.x = prev.x * -1;
+                holdingTwoHori = true;
+            }
+        } else
+        {
+            holdingTwoHori = false;
+            if(leftPressed)
+            {
+                move.x = -1;
+            }
+            if(rightPressed)
+            {
+                move.x = 1;
+            }
+        }
+        if (upPressed && downPressed)
+        {
+            if(holdingTwoVert)
+            {
+                move.y = prev.y;
+            } else
+            {
+                move.y = prev.y * -1;
+                holdingTwoVert = true;
+            }
+        } else
+        {
+            holdingTwoVert = false;
+            if(downPressed)
+            {
+                move.y = -1;
+            }
+            if(upPressed)
+            {
+                move.y = 1;
+            }
+        }
+        // player movement calculations end here ////////////////////////////////////////////////////////
     }
     
     void FixedUpdate()
     {
-        transform.position += move * speed * Time.deltaTime; // basic player movement
+        // player movement executed ////////////////////////////////////////////////////////
+        Vector3 increment = move * speed * Time.deltaTime;
+        Vector3 after = transform.position + increment;
+        // player object bounds  ////////////////////////////////////////////////////////
+        if (after.x < -13)
+        {
+            increment.x -= after.x+13;
+        }
+        if (after.x > 13)
+        {
+            increment.x -= after.x-13;
+        }
+        if (after.y < -9)
+        {
+            increment.y -= after.y+9;
+        }
+        if (after.y > 9)
+        {
+            increment.y -= after.y-9;
+        }
+        transform.position += increment;
     }
 }
