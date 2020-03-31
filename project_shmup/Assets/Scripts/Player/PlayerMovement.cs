cfdea33381d555f160f82ec240781388f15e2d32
Kenneth <kenneth@Admins-MBP-2.home>
2020-03-31 14:24:42 -0400

    test commit w/ unity

--- project_shmup/Assets/Scripts/Player/PlayerMovement.cs
+++ project_shmup/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    // this comment was made by Kenneth
     void Start()
     {
         
