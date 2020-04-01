e82563cf578411ef28b3d692908d2a17adad0eb4
MadeInChina121 <ethan.aristanto@verizon.net>
2020-04-01 17:55:22 -0400

    Update Enemy.cs

--- project_shmup/Assets/Scripts/Enemies/Enemy.cs
+++ project_shmup/Assets/Scripts/Enemies/Enemy.cs
@@ -31,6 +31,10 @@ public class Enemy : MonoBehaviour
         // Instantiate(deathEffect, transform.position, Quaternion.identity); // see above
         Destroy(gameObject); // remove the enemy when he die
     }
+    public void Shoot (float time, bullet)
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
