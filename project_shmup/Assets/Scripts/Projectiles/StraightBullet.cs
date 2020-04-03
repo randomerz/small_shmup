6cdf895892380b86cdd76d3e7c55b9c966660454
maxz2002 <mz13223@gmail.com>
2020-04-03 16:35:50 -0400

    straightbullet deletes at bottom of screen

--- project_shmup/Assets/Scripts/Projectiles/StraightBullet.cs
+++ project_shmup/Assets/Scripts/Projectiles/StraightBullet.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class StraightBullet : Projectile
 {
+
+    public float topBound = 9;
+    public float rightBound = 16;
+    public float leftBound = -16;
+    public float bottomBound = -9;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,9 @@ public class StraightBullet : Projectile
     void FixedUpdate()
     {
         transform.position += transform.up * speed * Time.deltaTime;
+        if (transform.position.x > rightBound || transform.position.x < leftBound || transform.position.y > topBound || transform.position.y < bottomBound)
+        {
+            Destroy(transform.gameObject);
+        }
     }
 }
