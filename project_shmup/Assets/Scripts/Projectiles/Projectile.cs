f466ecbd2d00845643f46031658d13a837b63631
jayakumarPawan <120random.things@gmail.com>
2020-04-03 19:10:58 -0400

    bug fix

--- project_shmup/Assets/Scripts/Projectiles/Projectile.cs
+++ project_shmup/Assets/Scripts/Projectiles/Projectile.cs
@@ -9,11 +9,9 @@ public class Projectile : MonoBehaviour
     public string path;
     public float damage;
     // Start is called before the first frame update
-    void Start(float s, string p, float d)
+    void Start()
     {
-        speed = s;
-        path = p;
-        damage = d;
+
     }
 
     // Update is called once per frame
