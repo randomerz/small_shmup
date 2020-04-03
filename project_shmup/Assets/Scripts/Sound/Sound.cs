651f8a3ab69826391e0db5f7e861abe7fbcd2a0d
MadeInChina121 <ethan.aristanto@verizon.net>
2020-04-03 19:05:41 -0400

    Finished SoundManager Object

--- project_shmup/Assets/Scripts/Sound/Sound.cs
+++ project_shmup/Assets/Scripts/Sound/Sound.cs
@@ -2,9 +2,19 @@
 using UnityEngine;
 
 [System.Serializable]
-public class Sound 
+public class Sound //initiallizes all fields for a sound, same for AudioSource.
 {
+    public string name;
     public AudioClip clip;
+    
+    [Range(0f, 1f)]
     public float volume;
+    [Range(.1f, 3f)]
     public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+
 }
