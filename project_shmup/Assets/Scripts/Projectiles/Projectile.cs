c72d86e00aa4a3b727e52c35ecd4af7d8480b75e
jayakumarPawan <120random.things@gmail.com>
2020-04-03 19:05:02 -0400

    added constructer for projectile

--- project_shmup/Assets/Scripts/Projectiles/Projectile.cs
+++ project_shmup/Assets/Scripts/Projectiles/Projectile.cs
@@ -7,10 +7,13 @@ public class Projectile : MonoBehaviour
 {
     public float speed;
     public string path;
+    public float damage;
     // Start is called before the first frame update
-    void Start()
+    void Start(float s, string p, float d)
     {
-        
+        speed = s;
+        path = p;
+        damage = d;
     }
 
     // Update is called once per frame
