5373b7abb64cb46bdb81183872b9a18f118c1454
MadeInChina121 <ethan.aristanto@verizon.net>
2020-04-03 18:26:01 -0400

    my own scene +little updates

---
+++ project_shmup/Assets/Scripts/Sound/Sound.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound 
+{
+    public AudioClip clip;
+    public float volume;
+    public float pitch;
+}
---
+++ project_shmup/Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    
+    /*public static void playSound()
+    {
+        GameObject gameSoundObject = new GameObject("Sound");
+        AudioSource audioSource = gameSoundObject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot();
+    }*/
+}
