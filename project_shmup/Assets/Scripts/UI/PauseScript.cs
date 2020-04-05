eefa82f16a3991991d914563e47e5a08ce9342fb
bernard0331 <47488073+bernard0331@users.noreply.github.com>
2020-04-05 14:41:34 -0400

    Made the pause menu

---
+++ project_shmup/Assets/Scripts/UI/PauseScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void MainMenu() 
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void SettingsMenu() 
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
