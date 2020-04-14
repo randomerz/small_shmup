9d748ba8395886c03918f0f08ce8ebdd1148e018
maxz2002 <mz13223@gmail.com>
2020-04-13 21:58:15 -0400

    added mine animation and script

---
+++ project_shmup/Assets/Scripts/Enemies/Mine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : Enemy
+{
+    public float timeToExplosion = 5.0f;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (timeToExplosion < 3.5)
+        {
+            Debug.LogWarning("timeToExplosion cannot be less than 3.5.");
+            timeToExplosion = 3.5f;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timeToExplosion -= Time.deltaTime;
+        if (timeToExplosion <= 3.5)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        animator.SetBool("explode", true);
+    }
+}
