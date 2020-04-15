566e930da32c570fffaa8c8afe4a0bc573c10c9d
MadeInChina121 <ethan.aristanto@verizon.net>
2020-04-15 17:14:23 -0400

    added scoring shit to my screen. updating works 80085 XD

---
+++ project_shmup/Assets/Scripts/ScoreUpdater.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ScoreManager scoreM;
+    public Text score;
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = scoreM.GetScore().ToString();
+    }
+}
