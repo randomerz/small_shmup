c622318966ebceb1d8207858e2ed55a30cd007c8
boomo <randomerz56@gmail.com>
2020-06-01 17:13:51 -0400

    installed TMP, added fonts, revamped landing scene

---
+++ project_shmup/Assets/Scripts/UI/LandingScreenController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LandingScreenController : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject levelSelectPanel;
+    public GameObject optionsPanel;
+
+    public bool isLevelSelectOpen = false;
+    public bool isOptionsOpen = false;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isLevelSelectOpen)
+                CloseLevelSelect();
+            if (isOptionsOpen)
+                CloseOptions();
+        }
+    }
+
+    public void OpenLevelSelect()
+    {
+        isLevelSelectOpen = true;
+        mainMenuPanel.SetActive(false);
+        levelSelectPanel.SetActive(true);
+    }
+
+    public void CloseLevelSelect()
+    {
+        isLevelSelectOpen = false;
+        mainMenuPanel.SetActive(true);
+        levelSelectPanel.SetActive(false);
+    }
+
+    public void OpenOptions()
+    {
+        isOptionsOpen = true;
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        isOptionsOpen = false;
+        mainMenuPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit(0);
+    }
+
+    //public void OpenGame()
+    //{
+    //    SceneManager.LoadScene("SampleScene");
+    //}
+}
