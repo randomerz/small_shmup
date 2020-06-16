71cf15bcd29fe94f07ac1e053a281b94a86fe729
boomo <randomerz56@gmail.com>
2020-06-16 16:12:55 -0400

    finished part of stage 2, added enemies, updated stage 1 to be easier
    - added orange/purple mines/minelayers and created override animation controllers for them
    - updated aimed enemies so that basic aiming would be round top and predictive would be trapezoid top
    - updated waves in area 1 to fit
    - added waves and parts to stage 2
    - added delay script to delay activation of other scripts, for intro parts

---
+++ project_shmup/Assets/Scripts/Enemies/DelayScripts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayScripts : MonoBehaviour
+{
+    public float delayTime = 1f;
+    private List<MonoBehaviour> scripts = new List<MonoBehaviour>();
+
+    void Start()
+    {
+        foreach (MonoBehaviour s in GetComponents<MonoBehaviour>())
+            if (s.enabled && s.GetType() != typeof(DelayScripts))
+            {
+                s.enabled = false;
+                scripts.Add(s);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (delayTime <= 0)
+        {
+            foreach (MonoBehaviour s in scripts)
+                s.enabled = true;
+            GetComponent<DelayScripts>().enabled = false;
+        }
+        delayTime -= Time.deltaTime;
+    }
+}
