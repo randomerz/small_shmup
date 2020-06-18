536f1d8613fb8dc3d3ae2b0523c1c65dde16f8a5
maxz2002 <mz13223@gmail.com>
2020-06-18 15:21:22 -0400

    Update to freighter

---
+++ project_shmup/Assets/Scripts/Enemies/Bosses/Freighter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freighter : MonoBehaviour
+{ 
+    public float startingX = 18;
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = new Vector3(startingX, 0, 0);
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 0)
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
