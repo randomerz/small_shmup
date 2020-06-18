c7ddc068784c529348a57b82dfb34036912cae94
Orange232 <kkt.dude74@gmail.com>
2020-06-18 11:42:20 -0400

    started rockets

---
+++ project_shmup/Assets/Scripts/Enemies/Rocket.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private float rotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public float FindAngle()
+    {
+    }
+}
