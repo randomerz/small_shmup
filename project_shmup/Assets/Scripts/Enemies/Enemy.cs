f7c18447f6c06b8c76538ed92043085ca37b0a5c
boomo <randomerz56@gmail.com>
2020-06-19 21:58:30 -0400

    added hitbox to freighter and make stuff flash black bc #blacklivesmatter

--- project_shmup/Assets/Scripts/Enemies/Enemy.cs
+++ project_shmup/Assets/Scripts/Enemies/Enemy.cs
@@ -12,6 +12,11 @@ public class Enemy : MonoBehaviour
     
     public float tempHealth = 0;
 
+    private bool strobing;
+    private const string SHADER_COLOR_NAME = "_Color";
+    private SpriteRenderer sprite;
+    private Material material;
+
     //public GameObject deathEffect; // we can use this to make a death effect when he explodes
 
     // Start is called before the first frame update
@@ -20,9 +25,10 @@ public class Enemy : MonoBehaviour
         if (transform.parent != null)
             wave = transform.parent.GetComponent<EnemyWave>();
         scoreManager = GameObject.Find("Main Camera").GetComponent<ScoreManager>();
+        sprite = GetComponent<SpriteRenderer>();
+        material = sprite.material;
     }
     
-
 
     public void TakeDamage()
     {
@@ -36,6 +42,10 @@ public class Enemy : MonoBehaviour
             if (health <= 0) // if the enemy runs out of health
             {
                 Die(); // die :^)
+            }
+            else
+            {
+                StrobeColor(1, Color.black);
             }
         }
     }
@@ -65,8 +75,45 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject); // remove the enemy when he die
     }
 
+    // code daniel copy pasted 
+    public void StrobeColor(int _strobeCount, Color _toStrobe)
+    {
+        if (strobing)
+            return;
 
-    
+        strobing = true;
+        
+        Color oldColor = sprite.color;
+
+        StartCoroutine(StrobeColorHelper(0, ((_strobeCount * 2) - 1), oldColor, _toStrobe));
+    }
+
+    public void StrobeAlpha(int _strobeCount, float a)
+    {
+        Color toStrobe = new Color(sprite.color.r, sprite.color.b, sprite.color.g, a);
+        StrobeColor(_strobeCount, toStrobe);
+    }
+
+    private IEnumerator StrobeColorHelper(int _i, int _stopAt, Color _color, Color _toStrobe)
+    {
+        if (_i <= _stopAt)
+        {
+            if (_i % 2 == 0)
+                material.SetColor(SHADER_COLOR_NAME, _toStrobe);
+            else
+                material.SetColor(SHADER_COLOR_NAME, _color);
+
+            yield return new WaitForSeconds(0.05f);
+            StartCoroutine(StrobeColorHelper((_i + 1), _stopAt, _color, _toStrobe));
+        }
+        else
+        {
+            strobing = false;
+        }
+    }
+
+
+
     void Update()
     {
         
