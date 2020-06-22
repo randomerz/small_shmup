77d36ad7e819df0f8202a27095b00b624e7b4a30
maxz2002 <mz13223@gmail.com>
2020-06-22 15:15:40 -0400

    front and back turrets now turn with player

--- project_shmup/Assets/Scripts/Enemies/Bosses/Rotation.cs
+++ project_shmup/Assets/Scripts/Enemies/Bosses/Rotation.cs
@@ -6,17 +6,21 @@ public class Rotation : MonoBehaviour
 {
 
     public Animator animator;
-    public WeaponShotgun weapon;
+    public PlayerMovement playerMovement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("frontTurretAngle", weapon.baseAngle);
+        float baseAngle;
+        Vector3 playerPos = playerMovement.transform.position;
+        baseAngle = Mathf.Atan2(playerPos.y - transform.position.y, playerPos.x - transform.position.x) * Mathf.Rad2Deg;
+        baseAngle -= 90; // idk why but this fixes it
+        animator.SetFloat("Angle", baseAngle);
     }
 }
