0f322564d1ff27d71a62e00a953222bacf2673f0
boomo <randomerz56@gmail.com>
2020-06-24 15:08:08 -0400

    added leantween library and tried animating some ui but its gay

---
+++ project_shmup/Assets/Scripts/UI/LT_Slide.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LT_Slide : MonoBehaviour
+{
+    public AnimationCurve curve;
+    public LeanTweenType inType;
+    public LeanTweenType outType;
+
+    public float newX = 0;
+    public float newY = 0;
+    public float time = 0.5f;
+
+    public void Slide()
+    {
+        LeanTween.moveLocal(gameObject, new Vector3(newX, newY, 0), time).setOnComplete(Close).setEase(outType);
+    }
+
+    public void Close()
+    {
+        Debug.Log("closed stats");
+    }
+}
